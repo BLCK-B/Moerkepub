3cae57de3da6ed45ac7f8a2154095b8c4f02dd15
BLCK <123077751+BLCK-B@users.noreply.github.com>
2024-10-30 18:17:14 +0100

    settings path

--- persistence.py
+++ persistence.py
@@ -3,25 +3,24 @@ import os
 import platform
 from pathlib import Path
 
-settings_file = 'settings.json'
-
 
 def load():
+    path = get_appdata_path() / 'settings.json'
     default_settings = {
         'selected_model': 'none',
         'selected_hw': 'cpu'
     }
-    if os.path.exists(settings_file):
-        with open(settings_file, 'r') as json_file:
+    if os.path.exists(path):
+        with open(path, 'r') as json_file:
             return json.load(json_file)
     else:
-        with open(settings_file, 'w') as json_file:
+        with open(path, 'w') as json_file:
             json.dump(default_settings, json_file, indent=4)
         return default_settings
 
 
 def __save__(settings):
-    with open(settings_file, 'w') as json_file:
+    with open(get_appdata_path() / 'settings.json', 'w') as json_file:
         json.dump(settings, json_file, indent=4)
 
 
