cd809fc2a22bd315fe18b0488e1331765b035f4b
BLCK <123077751+BLCK-B@users.noreply.github.com>
2024-10-30 17:53:56 +0100

    model

---
+++ models/model_small100.py
@@ -0,0 +1,33 @@
+import torch
+from transformers import M2M100ForConditionalGeneration
+from tokenization_small100 import M2M100Tokenizer
+import persistence
+
+
+def get_language_codes():
+    ref_tokenizer = M2M100Tokenizer.from_pretrained(str(persistence.get_appdata_path() / 'models' / 'small100-quantized'))
+    model_codes = dict()
+    for token in ref_tokenizer.additional_special_tokens:
+        model_codes[token] = token.replace('_', '')
+    return model_codes
+
+
+class Model:
+
+    def __init__(self, hw, source_lang, target_lang):
+        print("selected: ", source_lang, target_lang)
+        self.model = M2M100ForConditionalGeneration.from_pretrained(str(persistence.get_appdata_path() / 'models' / 'small100-quantized'))
+        if hw == "cuda":
+            self.device = torch.device("cuda:0")
+        elif hw == "cpu":
+            self.device = torch.device("cpu")
+        else:
+            raise ValueError("Device must be 'cuda' or 'cpu'")
+        self.tokenizer = M2M100Tokenizer.from_pretrained(str(persistence.get_appdata_path() / 'models' / 'small100-quantized'))
+        self.tokenizer.tgt_lang = target_lang.replace('_', '')
+
+    def batch_process(self, text):
+        encoded_hi = self.tokenizer(text, return_tensors="pt", padding=True, truncation=True).to(self.device)
+        generated_tokens = self.model.generate(**encoded_hi)
+        result = self.tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)
+        return result
